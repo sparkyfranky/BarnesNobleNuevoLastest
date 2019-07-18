5447413c774029ec79539fc1991bb911f3e99382
Wilson <fwwilson@amazon.com>
2019-07-18 10:01:52 -0600

    Combio a la funcion obtenerTarjetas en servicioTarjeta

--- public/js/servicioTarjeta.js
+++ public/js/servicioTarjeta.js
@@ -14,16 +14,34 @@ let registrarTarjeta = (pnombre1, ptipoTarjeta, pnumTarjeta, pexpiracion, pcvv )
     });
 };
 
-var obtenerTarjeta = async () => {
-    var response = await fetch('http://localhost:4000/api/listarTarjetas', {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json;charset=UTF-8',
-        }
-    });
-    var result = await response.json();
-    return result.listaTarjetas;
-}
+
+let obtenerTarjetas  = async () => {
+    try{ 
+        //Fetch data from an url endpoint:
+        const response = await axios({
+            method: 'get',
+            url: 'http://localhost:4000/api/listarTarjetas',  
+            responseType: 'json'   
+        });
+        const result = await response;
+        return result.data.listaTarjetas;
+    }catch (error){
+        console.log(error);
+    } 
+};
+
+
+// var obtenerTarjeta = async () => {
+//     var response = await fetch('http://localhost:4000/api/listarTarjetas', {
+//         method: 'get',
+//         headers: {
+//             'Content-Type': 'application/json;charset=UTF-8',
+//         }
+//     });
+    
+//     return result.listaTarjetas;
+// }
+
 
 
 
